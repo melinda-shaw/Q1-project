218e39f7c9138af9430a42611a91b6a545be737d
Melinda Shaw <melinda.shaw@me.com>
2017-10-02 12:57:04 -0700

    updating index and main files from test files

--- main.js
+++ main.js
@@ -1,34 +1,146 @@
-// // here's one thought
-// var q3 = {
-//   question: "dksldll";
-//   answer: [{
-//       yes: 'text her'
-//     },
-//     {
-//       no: 'loser'
-//     }
-//   ]
-// }
+var questions = [{
+    id: "q4",
+    class: 'alert alert-warning',
+    qText: "Is it 11:37 p.m. on her bday and you have been crazy busy all day and are now out and your battery is at 4%?",
+    response: [{
+      answer: "Yes",
+      text: "Text from a friend's phone. Save your battery in case you need to call security on the way back to your dorm."
+    }, {
+      answer: "No, it's at 8%.",
+      text: "Text now."
+    }]
+  },
+  {
+    id: "q5",
+    class: 'alert alert-info',
+    qText: "Did you just spot Malia Obama?",
+    response: [{
+      answer: "Yes",
+      text: "Text mom. But do not be such a creeper around Malia that she has to text Michelle."
+    }, {
+      answer: "Nope",
+      text: "Oh well."
+    }]
+  },
+  {
+    id: "q3",
+    class: 'alert alert-secondary',
+    qText: "Can you not find your umbrella and dental floss?",
+    response: [{
+      answer: "Yes",
+      text: "Text. She knows exactly where they are."
+    }, {
+      answer: "Maybe, but not planning to use either for next four years.",
+      text: "Do not text her this."
+    }]
+  },
+  {
+    id: "q6",
+    class: 'alert alert-danger',
+    qText: "Has it been more than 24 hours since she texted you with a non-urgent question?",
+    response: [{
+      answer: "Yes",
+      text: "Are you the first sibling to leave the nest?",
+      answer: [{
+        answer: "Yes",
+        text: "Text."
+      }, {
+        answer: "No",
+        text: "Text tomorrow."
+      }]
+    }, {
+      answer: "No",
+      text: "You can chill."
+    }]
+  },
+
+  {
+    id: "q2",
+    class: 'alert alert-dark',
+    qText: "Are your classes messed up?",
+    response: [{
+      answer: "Yes",
+      text: "Do you expect her to fix this?",
+      answer: [{
+        answer: "Yes",
+        text: "Alas, she cannot. It’s all you."
+      }, {
+        answer: "No",
+        text: "Do text for sympathy."
+      }]
+    }, {
+      answer: "No",
+      text: "Amazing! Text to tell her."
+    }]
+  },
+  {
+    id: "q1",
+    class: 'alert alert-warning',
+    qText: "Are you injured?",
+    response: [{
+      answer: "Yes",
+      text: "Did you seek medical help?",
+      responseLevel2: [{
+        answer: "Yes",
+        text: "Good. Now text."
+      }, {
+        answer: "No",
+        text: "Seek help or she will freak. : )"
+      }]
+    }, {
+      answer: "No",
+      text: "Whew! Text away."
+    }, {
+      answer: "Yes, but only your feelings.",
+      text: "Text and maybe call. !<3!"
+    }]
+  }
+]
 
 
-// function toggler(divId) {
-//   $("#" + divId).toggle();
-// }
+///// ======================================
+var rowOpen = `<div class="main-questions row d-flex align-items-center ">`
+var ColOpen = `<div class="main-questions col text-center ">`
+var divClose = `</div>`
+var br = `</br>`
 
-// https://getbootstrap.com/docs/4.0/components/alerts/#events
-// Event	Description
-// close.bs.alert	This event fires immediately when the close instance method is called.
-// closed.bs.alert	This event is fired when the alert has been closed (will wait for CSS transitions to complete).
 
-// $('#myAlert').on('closed.bs.alert', function() {
-//   // do something…
-// })
+//APPEND OBJECTS INTO BODY
+function appendMain() {
+  $('.main-questions').empty()
+  for (var i = 0; i < questions.length; i++) {
+    $('.main-questions').append(
+      `<div id='${questions[i].id}' class="${questions[i].class}"  onClick="display('${questions[i].id}')" role="alert">` +
+      `<a  href="# " class="alert-link ">${questions[i].qText}</a>` +
+      divClose)
+  }
+}
+appendMain()
 
-// function myFunction() {
-//   var x = document.getElementById('myDIV');
-//   if (x.style.display === 'none') {
-//     x.style.display = 'block';
-//   } else {
-//     x.style.display = 'none';
-//   }
-// }
+function display(id) {
+  for (var i = 0; i < questions.length; i++) {
+    if (questions[i].id === id) {
+      var question = questions[i]
+
+    }
+  }
+
+  $('.main-questions').empty();
+  $('.main-questions').append(
+    `<div class="row d-flex align-items-center">` + `
+  <div class="col-12 text-center ">` +
+    `<div class="alert alert-warning" role="alert">` + `${question.qText}` + divClose + divClose + divClose)
+
+  $('.main-questions').append(
+    `<div class="row ">` + `<div class="col-6  text-center">` + `<button data-toggle="collapse" data-target="#yes-answer" type="button" class="btn btn-warning">  ${question.response[0].answer}  </button>` +
+    `<div id='yes-answer' style=display-hidden class="collapse alert alert-info" role="alert">` + question.response[0].text +
+    divClose + divClose + `<div class="col-6  text-center">` + `<button data-toggle="collapse" data-target="#no-answer" type="button" class="btn btn-warning">  ${question.response[1].answer}  </button>` +
+    `<div id='no-answer' style=display-hidden class="collapse alert alert-info" role="alert">` + question.response[1].text +
+    divClose + divClose + divClose + divClose
+  )
+
+  $('.main-questions').append(
+    `<div class="row ">` + `<div class="col-12 d-flex align-items-center">` + `<a href="#" onClick='appendMain()' id='bottom-button' class="btn btn-dark btn-lg active" role="button" aria-pressed="true">back to questions</a>` + divClose + divClose)
+
+
+}
