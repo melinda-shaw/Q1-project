753566d96b0c334157bd3f00dac9e1a26e5bca04
Melinda Shaw <melinda.shaw@me.com>
2017-10-04 15:23:53 -0700

    re-made JS file

--- moreComplexQuestions.js
+++ moreComplexQuestions.js
@@ -4,18 +4,49 @@
   image2: "",
   class: 'alert alert-danger',
   qText: "Has it been more than 24 hours since she texted you with a non-urgent question?",
-  response: [{
-    answer: "Yes",
-    text: "Are you the first sibling to leave the nest?",
-    responseLevel2: [{
+  responses: [{
       answer: "Yes",
-      text: "Text."
-    }, {
-      answer: "No",
-      text: "Text tomorrow."
-    }]
-  }, {
-    answer: "No",
-    text: "You can chill."
-  }]
+      question: {
+        qText: 'test nested q'
+      }
+    },
+    {
+      answer: "no",
+      question: {
+        qText: 'test nested q'
+        responses: [{
+            answer: "Yes",
+            question: {
+              qText: 'test nested q'
+            }
+          },
+          {
+            answer: "No",
+            question: {
+              qText: 'test nested q'
+            }
+          }
+        ]
+      }
+    }
+  ]
+}
+
+var q1 = {
+  qText: "Has it been more than 24 hours since she texted you with a non-urgent question?",
+  responses: [yes, no]
+}
+
+var q2 = {
+  qText: "Q2?",
+  responses: []
+}
+
+var yes = {
+  bText = 'Yes'
+}
+
+var no = {
+  bText: 'No',
+  question: q2
 }
